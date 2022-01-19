e08609b80c847e1de942f9d48c3d4dd56dcb1e6f
Thomas Leonard <talex5@gmail.com>
2022-01-19 08:51:51 +0000

    Disable openssl for now
    It segfaults under multicore: see https://github.com/savonet/ocaml-ssl/issues/76

--- src/http/http.ml
+++ src/http/http.ml
@@ -314,6 +314,8 @@ let no_tls = {
 }
 
 let openssl = {
+  create_handler = fun ~certificate_file:_ -> failwith "https://github.com/savonet/ocaml-ssl/issues/76"
+(*
   create_handler = begin fun
       ~certificate_file ~key_file
       ~handler
@@ -369,6 +371,7 @@ let openssl = {
         | Some _ ->
           assert false
   end;
+*)
 }
 
 (* TODO LATER Add ALPN + HTTP/2.0 with ocaml-tls, too. *)
@@ -647,7 +650,7 @@ let serve
     ~port
     ~stop
     ~error_handler
-    ~https:(if https then `OpenSSL else `No)
+    ~https:(if https then `OCaml_TLS else `No)
     ?certificate_file
     ?key_file
     ?certificate_string:None
@@ -739,7 +742,7 @@ let run
         ~port
         ~stop
         ~error_handler
-        ~https:(if https then `OpenSSL else `No)
+        ~https:(if https then `OCaml_TLS else `No)
         ?certificate_file ?key_file
         ?certificate_string:None ?key_string:None
         ~builtins
